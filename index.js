cecf76e0ddec276af8de230875417e14566b51ca
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-01-05 23:05:15 +1100

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(someValue) {
+    return Math.abs(someValue - 42);
+  }
+
+  function distanceFromHqInFeet(someValue) {
+    return distanceFromHqInBlocks(someValue) * 264;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    let distanceInBlocks = Math.abs(destination - start);
+    let distanceInFeet = distanceInBlocks * 264;
+    return distanceInFeet;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    let distanceInFeet = distanceTravelledInFeet(start, destination)
+    if (distanceInFeet <= 400) {
+      return 0;
+    } else if (distanceInFeet > 400 && distanceInFeet <= 2000) {
+      // Price for distance between 400 and 2000 feet (2 cents per foot)
+      return (distanceInFeet - 400) * 0.02;
+      } else if (distanceInFeet > 2000 && distanceInFeet < 2500) {
+      // Flat fare for distance over 2000 feet and under 2500 feet
+      return 25;
+  } else if (distanceInFeet >= 2500) {
+      return 'cannot travel that far';
+  }
+}
